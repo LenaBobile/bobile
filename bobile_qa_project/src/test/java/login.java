7788e04420f8bbf954689f93d71db1a6bda1aa3d
Lena Guterman <lena@bobile.com>
2018-09-17 15:40:05 +0300

    Refactoring of login class- methods and parametrs

--- bobile_qa_project/src/test/java/login.java
+++ bobile_qa_project/src/test/java/login.java
@@ -1,18 +1,12 @@
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class login {
     //FirefoxDriver wd;
@@ -27,16 +21,32 @@ public class login {
 
     @Test
     public void login() {
-        wd.get("https://sandbox.bobile.com/admin/credential/login/");
-        wd.findElement(By.id("username")).click();
-        wd.findElement(By.id("username")).clear();
-        wd.findElement(By.id("username")).sendKeys("lenadag@bobile.com");
-        wd.findElement(By.id("password")).click();
-        wd.findElement(By.id("password")).clear();
-        wd.findElement(By.id("password")).sendKeys("123456");
+        openSite("https://sandbox.bobile.com/admin/credential/login/");
+        enterUserName("lenadag@bobile.com");
+        enterPassword("123456");
+        clickLoginBtn();
+    }
+
+    public void clickLoginBtn() {
         wd.findElement(By.id("login")).click();
     }
-    
+
+    public void enterPassword(String password) {
+        wd.findElement(By.id("password")).click();
+        wd.findElement(By.id("password")).clear();
+        wd.findElement(By.id("password")).sendKeys(password);
+    }
+
+    public void enterUserName(String username) {
+        wd.findElement(By.id("username")).click();
+        wd.findElement(By.id("username")).clear();
+        wd.findElement(By.id("username")).sendKeys(username);
+    }
+
+    public void openSite(String site) {
+        wd.get(site);
+    }
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
