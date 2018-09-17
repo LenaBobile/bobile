06da659c82bfb7714c0a789f7780d5d4d71e02c1
Lena Guterman <lena@bobile.com>
2018-09-17 12:15:52 +0300

    class login
    class login created

---
+++ bobile_qa_project/src/test/java/login.java
@@ -0,0 +1,49 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class login {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void login() {
+        wd.get("https://sandbox.bobile.com/admin/credential/login/");
+        wd.findElement(By.id("username")).click();
+        wd.findElement(By.id("username")).clear();
+        wd.findElement(By.id("username")).sendKeys("lenadag@bobile.com");
+        wd.findElement(By.id("password")).click();
+        wd.findElement(By.id("password")).clear();
+        wd.findElement(By.id("password")).sendKeys("123456");
+        wd.findElement(By.id("login")).click();
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
